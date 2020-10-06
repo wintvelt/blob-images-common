b375c0749861025dc64c725358c38b5868086af3
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-06 17:53:34 +0200

    fix cognito delete

--- core/cognito.js
+++ core/cognito.js
@@ -2,11 +2,16 @@ import AWS from "aws-sdk";
 
 var cognito = new AWS.CognitoIdentityServiceProvider({
     apiVersion: "2016-04-18",
-    params: {
-        UserPoolId: process.env.userPoolId || process.env.devUserPoolId
-    }
+    // params: {
+    //     UserPoolId: process.env.userPoolId || process.env.devUserPoolId
+    // }
 });
 
-export const disableUser = (userId) => (
-    cognito.adminDisableUser({ Username: userId }).promise()
-);
+export const disableUser = (userId) => {
+    const params = {
+        UserPoolId: process.env.userPoolId || process.env.devUserPoolId,
+        Username: userId.slice(1)
+    };
+    console.log(params);
+    return cognito.adminDisableUser(params).promise();
+};
