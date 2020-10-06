0af759845f6609f753a5ac16547582ece316e774
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-06 16:47:10 +0200

    bugfix

--- core/cognito.js
+++ core/cognito.js
@@ -2,11 +2,11 @@ import AWS from "aws-sdk";
 
 var cognito = new AWS.CognitoIdentityServiceProvider({
     apiVersion: "2016-04-18",
-    params: { 
+    params: {
         UserPoolId: process.env.userPoolId || process.env.devUserPoolId
     }
 });
 
 export const disableUser = (userId) => (
-    cognito.adminDisableUser({Username}).promise()
+    cognito.adminDisableUser({ Username: userId }).promise()
 );
