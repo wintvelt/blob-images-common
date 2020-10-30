cc9052bf3e8d0757a5e955c0cb12964763315779
Wouter <wintvelt@me.com>
2020-10-30 16:35:46 +0100

    add featureId

--- core/ids.js
+++ core/ids.js
@@ -3,3 +3,4 @@ import { nanoid } from 'nanoid';
 export const newPhotoId = () => 'P' + nanoid(15);
 export const newGroupId = () => 'G' + nanoid(15);
 export const newAlbumId = () => 'A' + nanoid(15);
+export const newFeatueId = () => 'F' + nanoid(15);
