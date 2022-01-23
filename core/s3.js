a290454fcfad17daf698b8d4b1344a71615053c0
Wouter <wintvelt@me.com>
2022-01-23 22:22:07 +0100

    add copyObject to S3 and set type module

--- core/s3.js
+++ core/s3.js
@@ -12,6 +12,7 @@ export const s3 = {
     delete: (params) => S3.deleteObject(params).promise(),
     get: (params) => S3.getObject(params).promise(),
     list: (params) => S3.listObjectsV2(params).promise(),
+    copyObject: (params) => S3.copyObject(params).promise(),
     getMetadata: (params) => S3.headObject(params).promise(),
     getSignedUrl: (params) => S3.getSignedUrl('putObject', params),
     getSignedUrlGet: (params) => S3.getSignedUrl('getObject', params)
