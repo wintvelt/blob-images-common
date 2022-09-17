b6c08fb27f742513614e75be76bbf4561517194b
Wouter <wintvelt@me.com>
2022-09-17 13:53:20 +0200

    remove ? from objects

--- core/handler.js
+++ core/handler.js
@@ -23,9 +23,13 @@ export const handler = (lambda) => {
 }
 
 export const getUserFromEvent = (event) => {
-    const baseAuthProvider = event.requestContext.identity?.cognitoAuthenticationProvider;
+    const baseAuthProvider = event.requestContext.identity
+        && event.requestContext.identity.cognitoAuthenticationProvider;
     // works differently for calls from node, maybe because http API works differently with sigv4Client
-    const altApiIdentityAmr = event.requestContext.authorizer?.iam?.cognitoIdentity?.amr;
+    const altApiIdentityAmr = event.requestContext.authorizer
+        && event.requestContext.authorizer.iam
+        && event.requestContext.authorizer.iam.cognitoIdentity
+        && event.requestContext.authorizer.iam.cognitoIdentity.amr;
     const altAuthProvider = (altApiIdentityAmr && altApiIdentityAmr.length === 3 && altApiIdentityAmr[2]);
 
     const authProvider = baseAuthProvider || altAuthProvider;
