9d9abb73320f09171f2f8e9e2266d02cf513c3ef
Wouter <wintvelt@me.com>
2023-02-13 16:58:26 +0100

    add put object to s3

--- core/s3.js
+++ core/s3.js
@@ -11,6 +11,7 @@ var S3 = new AWS.S3({
 export const s3 = {
     delete: (params) => S3.deleteObject(params).promise(),
     get: (params) => S3.getObject(params).promise(),
+    put: (params) => S3.putObject(params).promise(),
     list: (params) => S3.listObjectsV2(params).promise(),
     copyObject: (params) => S3.copyObject(params).promise(),
     getMetadata: (params) => S3.headObject(params).promise(),
